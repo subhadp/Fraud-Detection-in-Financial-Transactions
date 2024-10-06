537e13bb927390d1228d76a7fc76707ed2aa9518
Subhadp <subhadeep.roy.23@gmail.com>
2024-10-06 21:42:37 +0530

    Changes

--- src/model/predict_model.py
+++ src/model/predict_model.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import joblib
+from sklearn.preprocessing import StandardScaler
 
 MODEL_FILENAMES = {
     "LOG": r".\.\models\logistic_regression_model.pkl",
-    "DNN": r".\.\models\DNN_model.pkl",
-    "DS": r".\.\models\decision_tree_model.pkl"
+    "DS": r".\.\models\decision_tree_model.pkl",
+    "CB": r".\.\models\CatBoost_Model.pkl"
 }
 
 models = {name: joblib.load(path) for name, path in MODEL_FILENAMES.items()}
