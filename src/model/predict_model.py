cb0d3d6f5b5fbd888bedf8c797e00162204d932d
Subhadp <subhadeep.roy.23@gmail.com>
2024-10-09 12:15:16 +0530

    final notebook

--- src/model/predict_model.py
+++ src/model/predict_model.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler
 MODEL_FILENAMES = {
     "LOG": r".\.\models\logistic_regression_model.pkl",
     "DS": r".\.\models\decision_tree_model.pkl",
-    "CB": r".\.\models\CatBoost_Model.pkl"
+    "CAT": r".\.\models\CatBoost_Model.pkl"
 }
 
 models = {name: joblib.load(path) for name, path in MODEL_FILENAMES.items()}
